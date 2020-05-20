45e8afd5fb2f2067cc676675db29c750e4f2a8de
rahul <ragul880@gmail.com>
2020-05-20 11:53:05 +0530

    Updates

---
+++ precache-manifest.325d907269fe51b099e81ef14bf6e8a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b722ad98f0f796d4697f7124435f06d4",
+    "url": "/monster-roledex/index.html"
+  },
+  {
+    "revision": "b4931446d6bb75321722",
+    "url": "/monster-roledex/static/css/main.9e441c36.chunk.css"
+  },
+  {
+    "revision": "bac8629d46ff505a9177",
+    "url": "/monster-roledex/static/js/2.69c919c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-roledex/static/js/2.69c919c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4931446d6bb75321722",
+    "url": "/monster-roledex/static/js/main.b51a7d29.chunk.js"
+  },
+  {
+    "revision": "a40897668b5059357630",
+    "url": "/monster-roledex/static/js/runtime-main.d55f817a.js"
+  }
+]);
